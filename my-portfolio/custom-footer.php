b66630107749118283a1880c06ca5aba42adb7aa
Mirko D'Agnese <mirkoddesigner@gmail.com>
2021-06-07 20:19:33 +0200

    ~

--- my-portfolio/custom-footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-<div class="text-footer">
-	<p>• All works © Mirko D’Agnese. / Please do not reproduce without the expressed written consent • mirkoddesigner@gmail.com •</p></div>
-</footer>
