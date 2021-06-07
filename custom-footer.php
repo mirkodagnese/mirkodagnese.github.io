aa44b85f57a9e3c1d781bb2b7913723981c3c214
Mirko D'Agnese <mirkoddesigner@gmail.com>
2021-06-08 00:08:45 +0200

    ciaaaao biiirbaaaa

--- custom-footer.php
+++ custom-footer.php
@@ -1,4 +1,7 @@
+<div class="wrapper">
+    <div class="push"></div>
+</div>
 <footer>
     <div class="text-footer">
-        <p>• All works © Mirko D’Agnese. / Please do not reproduce without the expressed written consent • mirkoddesigner@gmail.com •</p></div>
+        <p>• All works © Mirko D’Agnese • Please do not reproduce without the expressed written consent • mirkoddesigner@gmail.com •</p></div>
 </footer>
